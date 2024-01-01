a576769b6e1515eeaeebf8fb01127963b1097c1e
zacred0rryn <zacred0rryn@proton.me>
2024-01-02 00:16:10 +0800

    initial commit

---
+++ convert.py
@@ -0,0 +1,27 @@
+import yaml
+import json
+import os
+
+def convert_yaml_to_json(yaml_file, json_file):
+  with open(yaml_file, 'r') as yf:
+    yaml_data = yaml.safe_load(yf)
+
+  json_data = {
+    "version": 1,
+    "rules": [
+      {
+        "domain": [item.split(',')[1] for item in yaml_data['payload'] if item.startswith('DOMAIN,')],
+        "domain_suffix": ['.' + item.split(',')[1] for item in yaml_data['payload'] if item.startswith('DOMAIN-SUFFIX,')],
+        "ip_cidr": [item.split(',')[1] for item in yaml_data['payload'] if item.startswith(('IP-CIDR,', 'IP-CIDR6,'))]
+      }
+    ]
+  }
+
+  with open(json_file, 'w') as jf:
+    json.dump(json_data, jf, indent=4)
+
+if __name__ == '__main__':
+  for yaml_file in os.listdir('.'):
+    if yaml_file.endswith('.yaml'):
+      new_json_file = 'ip-attribution-' + yaml_file.replace('rule-provider-', '').replace('.yaml', '.json')
+      convert_yaml_to_json(yaml_file, new_json_file)
