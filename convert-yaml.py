4a9ef2e1729ee98644cc315f8b5dfa98cdbb8157
Sakurado Rin <zacred0rryn@proton.me>
2025-04-29 10:49:16 +0800

    fix: Rule Provider compatibility

--- convert-yaml.py
+++ convert-yaml.py
@@ -5,18 +5,31 @@ import os
 def convert_yaml_to_json(yaml_file, json_file):
   with open(yaml_file, 'r') as yf:
     yaml_data = yaml.safe_load(yf)
-
+  
+  domains = []
+  domain_suffixes = []
+  ip_cidrs = []
+  
+  for item in yaml_data['payload']:
+    parts = item.split(',')
+    if item.startswith('DOMAIN,'):
+      domains.append(parts[1])
+    elif item.startswith('DOMAIN-SUFFIX,'):
+      domain_suffixes.append(parts[1])
+    elif item.startswith(('IP-CIDR,', 'IP-CIDR6,')):
+      ip_cidrs.append(parts[1])
+  
   json_data = {
     "version": 2,
     "rules": [
       {
-        "domain": [item.split(',')[1] for item in yaml_data['payload'] if item.startswith('DOMAIN,')],
-        "domain_suffix": [item.split(',')[1] for item in yaml_data['payload'] if item.startswith('DOMAIN-SUFFIX,')],
-        "ip_cidr": [item.split(',')[1] for item in yaml_data['payload'] if item.startswith(('IP-CIDR,', 'IP-CIDR6,'))]
+        "domain": domains,
+        "domain_suffix": domain_suffixes,
+        "ip_cidr": ip_cidrs
       }
     ]
   }
-
+  
   with open(json_file, 'w') as jf:
     json.dump(json_data, jf, indent=4)
 
